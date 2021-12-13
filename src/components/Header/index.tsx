b0fbe3636b119c667a23ed55c09e71aaa8cd4924
felixstenian <felixstenian@gmail.com>
2021-12-13 11:25:21 -0300

    add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
-}
+import styles from './header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div>
+        <img src="/Logo.svg" alt="logo" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
